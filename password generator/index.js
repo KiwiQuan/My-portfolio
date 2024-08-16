61a38fd6bdbc10577337d8463ce083d607e77bbe
ZeeQ <kw133875@gmail.com>
2024-08-15 22:32:05 -0500

    password

--- password generator/index.js
+++ password generator/index.js
@@ -1,41 +1,47 @@
-function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
 
-    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz"
-    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    const numberChars = "0123456789"
-    const symbolChars = "!@#$%^&*()-_=+\}][{/?.>,<;:`~"
+function generateBtn(){
+   function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
 
-    let allowedChars = ""
-    let password = ""
+      const lowercaseChars = "abcdefghijklmnopqrstuvwxyz"
+      const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      const numberChars = "0123456789"
+      const symbolChars = "!@#$%^&*()-_=+\}][{/?.>,<;:`~"
+  
+      let allowedChars = ""
+      let password = ""
+  
+      allowedChars += includeLowercase ? lowercaseChars : ""
+      allowedChars += includeUppercase ? uppercaseChars : ""
+      allowedChars += includeNumbers ? numberChars : ""
+      allowedChars += includeSymbols ? symbolChars : ""
+  
+     if(length <= 0 ){
+  return `(password length must be at least 1)`
+     }
+  
+     if( allowedChars.length === 0){
+  return `(at least 1 of the options need to be selected)`
+     }
+  
+     for(let i = 0; i < length; i++){
+  const randomIndex = Math.floor(Math.random() * allowedChars.length)
+  password += allowedChars[randomIndex]
+     }
+  
+      return password
+  }
 
-    allowedChars += includeLowercase ? lowercaseChars : ""
-    allowedChars += includeUppercase ? uppercaseChars : ""
-    allowedChars += includeNumbers ? numberChars : ""
-    allowedChars += includeSymbols ? symbolChars : ""
 
-   if(length <= 0 ){
-return `(password length must be at least 1)`
-   }
+   const passwordLength = document.getElementById("length").value
+   const includeLowercase = document.getElementById("lowercase").checked
+   const includeUppercase = document.getElementById("uppercase").checked
+   const includeNumbers = document.getElementById("numbers").checked
+   const includeSymbols = document.getElementById("symbols").checked  
+   const passwordText = document.getElementById("password")
+   
 
-   if( allowedChars.length === 0){
-return `(at least 1 set of characters need to be selected)`
-   }
 
-   for(let i = 0; i < length; i++){
-const randomIndex = Math.floor(Math.random() * allowedChars.length)
-password += allowedChars[randomIndex]
-   }
-
-    return password
+   const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols)
+   passwordText.textContent = `password: ${password}`
 }
 
-const passwordLength = 12
-const includeLowercase = true
-const includeUppercase = true
-const includeNumbers = true
-const includeSymbols = true  
-const passwordText = document.getElementById("password")
-
-const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols)
-passwordText.textContent = `password: ${password}`
-console.log(`Generated password: ${password}`)
