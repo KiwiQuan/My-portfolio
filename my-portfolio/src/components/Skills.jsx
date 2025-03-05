e110332f50b574f060d701de909b41012af361a3
ZeeQ <kw133875@gmail.com>
2025-03-05 15:10:48 -0600

    added icons to skills list

--- my-portfolio/src/components/Skills.jsx
+++ my-portfolio/src/components/Skills.jsx
@@ -1,6 +1,17 @@
+import { FaHtml5, FaCss3Alt, FaJs, FaPython } from "react-icons/fa";
+import { SiJquery, SiBootstrap, SiFlask, SiSqlite, SiReact } from "react-icons/si";
+
 const Skills = () => {
-  const languages = ["HTML", "CSS", "JavaScript", "Python"];
-  const technologies = ["JQuery", "Bootstrap", "Flask", "SQLite", "React"];
+  const languages = [{name: "HTML", icon: <FaHtml5 />},
+                     {name: "CSS", icon: <FaCss3Alt />}, 
+                     {name: "JavaScript", icon: <FaJs />}, 
+                     {name: "Python", icon: <FaPython />}];
+  
+  const technologies = [{name: "Javascript", icon: <FaJs />}, 
+                        {name: "Bootstrap", icon: <SiBootstrap />}, 
+                        {name: "Flask", icon: <SiFlask />}, 
+                        {name: "SQLite", icon: <SiSqlite />}, 
+                        {name: "React", icon: <SiReact />}];
 
   return (
     <section className="skills">
@@ -8,7 +19,7 @@ const Skills = () => {
         <h2>Languages I Know</h2>
         <ul className="skills__list">
           {languages.map((lang) => (
-            <li key={lang}>{lang}</li>
+            <li key={lang.name}>{lang.icon} {lang.name}</li>
           ))}
         </ul>
       </article>
@@ -17,7 +28,7 @@ const Skills = () => {
         <h2>Technologies I Know</h2>
         <ul className="skills__list">
           {technologies.map((tech) => (
-            <li key={tech}>{tech}</li>
+            <li key={tech.name}>{tech.icon} {tech.name}</li>
           ))}
         </ul>
       </article>
